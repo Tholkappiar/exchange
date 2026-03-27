b0d441e0e1fa522fbb520bb7b66e86400ebb7402
tholkappiar <tholkappiar2003@gmail.com>
2026-03-27 09:02:28 +0530

    feat(backend): clerk in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,9 +4,12 @@ import express from "express";
 import { orderRouter } from "./routes/order";
 import { RedisManager } from "./redis/RedisManager";
 import { WebsocketManager } from "./websocket/WebsocketManager";
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
 app.use(express.json());
+
+app.use(clerkMiddleware());
 
 // todo: refac these singleton workers - because we are only going to call this once somewhere
 RedisManager.getInstance().then((r) => r?.startWorker());
